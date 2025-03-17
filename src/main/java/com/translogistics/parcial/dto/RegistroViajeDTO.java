fae4473d4be2ded092c5d03050b14c064651e02e
GabrielNinoA <gabriel.nino02@uptc.edu.co>
2025-03-16 19:42:11 -0500

    Corregir registroDTO

--- src/main/java/com/translogistics/parcial/dto/RegistroViajeDTO.java
+++ src/main/java/com/translogistics/parcial/dto/RegistroViajeDTO.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class RegistroViaje {
+public class RegistroViajeDTO {
 
     private int id;
     private String fechaViaje;
